7328d3ec5d7b2596ffb326213dd58bdf7567ba97
Laixander Naguit <laixander@gmail.com>
2025-04-24 12:27:02 +0800

    Updates

--- pages/technologies.vue
+++ pages/technologies.vue
@@ -8,38 +8,37 @@
         </UContainer>
     </AppBanner>
     <AppSection>
-        <UContainer class="py-4 lg:py-20 space-y-20">
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-10">
-                <Feature 
-                    v-for="(feature, index) in items" 
-                    :key="index" 
-                    v-bind="feature"
-                    unstyled
-                    iconSize="w-14 h-14"
-                />
-            </div>
-
-            <div>
-                <FancyHeading 
-                    title="Why Choose Our Technology Stack?"
-                    align="center"
-                    size="text-3xl"
-                />
-                <FancyDivider align="center" class="mb-10" />
-                <div class="grid grid-cols-1 lg:grid-cols-3 gap-10">
-                    <UCard v-for="(feature, index) in reasons" 
-                    :key="index" >
-                        <Feature 
-                            v-bind="feature"
-                            align="center"
-                        />
-                    </UCard>
-                </div>
-            </div>
+        <UContainer class="grid grid-cols-1 lg:grid-cols-3 gap-10 py-4 lg:py-20">
+            <Feature 
+                v-for="(feature, index) in items" 
+                :key="index" 
+                v-bind="feature"
+                unstyled
+                iconSize="w-14 h-14"
+            />
         </UContainer>
     </AppSection>
     <AppSection>
-        <AppHighlights />
+        <UContainer class="text-center py-4 lg:py-20">
+            <FancyHeading 
+                title="Why Choose Our Technology Stack?"
+                align="center"
+                size="text-3xl"
+            />
+            <FancyDivider align="center" class="mb-10" />
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-10">
+                <UCard 
+                    v-for="(feature, index) in reasons" 
+                    :key="index" 
+                    :ui="{background: 'dark:bg-gray-950'}"
+                >
+                    <Feature 
+                        v-bind="feature"
+                        align="center"
+                    />
+                </UCard>
+            </div>
+        </UContainer>
     </AppSection>
 </template>
 
