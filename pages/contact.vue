c4d7545948e2002f172e96e87899cde52c08f82f
Laixander Naguit <laixander@gmail.com>
2025-04-28 16:50:26 +0800

    Update

--- pages/contact.vue
+++ pages/contact.vue
@@ -12,7 +12,7 @@
          <UContainer class="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-10 py-10 lg:py-20">
                 <ContactForm />
                 <div class="grid gap-6 *:rounded-xl *:p-6 *:flex *:flex-col *:justify-center">
-                    <div class="bg-primary-50">
+                    <div class="bg-primary-50 dark:bg-primary-950/50">
                         <FancyHeading title="Contact Information" size="text-xl" margin="mb-6" />
                         <ul class="space-y-1.5">
                             <li><UButton label="(632) 8538-5056" icon="i-lucide-phone" variant="link" size="xl" :padded="false" /></li>
@@ -20,9 +20,9 @@
                             <li><UButton label="Live Chat Available" icon="i-lucide-message-square" variant="link" size="xl" :padded="false" /></li>
                         </ul>
                     </div>
-                    <div class="bg-gray-50">
+                    <div class="bg-gray-50 dark:bg-gray-900">
                         <FancyHeading title="Office Hours" size="text-xl" margin="mb-4" />
-                        <ul class="text-gray-500 dark:text-gray-100 space-y-1">
+                        <ul class="font-light text-gray-500 dark:text-gray-400 space-y-1">
                             <li>Monday - Friday: 8:00 AM - 5:00 PM</li>
                             <li>Saturday: 8:00 AM - 12:00 PM</li>
                             <li>Sunday: Closed</li>
