6d1993ff65ba65e31066da313af29b43d2499365
Laixander Naguit <laixander@gmail.com>
2025-04-30 10:24:11 +0800

    Updates

--- components/Map.vue
+++ components/Map.vue
@@ -19,7 +19,7 @@
         class="absolute top-0 left-0 w-full h-full"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.1338458882262!2d121.01674635803198!3d14.55621982434462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9a4ad5a62ef%3A0xc2765d4370d6f7d4!2sRufino%20Pacific%20Tower%2C%2037th%20Floor%2C%20678%20Ayala%20Ave%2C%20Legazpi%20Village%2C%20Makati%2C%201220%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1712800573432!5m2!1sen!2sph"
         style="border:0;"
-        allowfullscreen=""
+        allowfullscreen="false"
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
         @load="mapLoaded"
--- pages/projects.vue
+++ pages/projects.vue
@@ -8,7 +8,17 @@
         </UContainer>
     </AppBanner>
     <AppSection>
-        <ComingSoon />
+        <!-- <ComingSoon /> -->
+         <UContainer class="py-10 lg:py-20">
+            <UTabs :items="items" :ui="uiTabsConfig">
+                <template #client>
+                    Client Projects
+                </template>
+                <template #internal>
+                    Internal Projects
+                </template>
+            </UTabs>
+         </UContainer>
     </AppSection>
     <AppSection>
         <AppHighlights />
@@ -17,4 +27,37 @@
 
 <script setup lang="ts">
 import { banner } from '@/data/projects'
+
+const items = [{
+  slot: 'client',
+  label: 'Client Projects',
+  icon: 'i-lucide-user-round'
+}, {
+  slot: 'internal',
+  label: 'Internal Projects',
+  icon: 'i-lucide-building-2'
+}]
+
+const uiTabsConfig = {
+    wrapper: 'space-y-8 flex flex-col justify-center w-full',
+    list: { 
+        base: 'mx-auto',
+        rounded: 'rounded-full',
+        height: 'h-13',
+        width: 'w-auto',
+        marker: {
+            background: 'bg-primary dark:bg-primary-900',
+            rounded: 'rounded-full',
+        },
+        tab: {
+            active: 'text-white dark:text-white',
+            height: 'h-11',
+            padding: 'px-5',
+            size: 'text-base',
+            font: 'font-light',
+            rounded: 'rounded-full',
+            icon: 'w-5 h-5 me-3'
+        }
+    }
+}
 </script>
