d83127bad9e48b949a5e7239abf094ea5a4260f9
laixander <laixander@gmail.com>
2025-05-12 21:45:06 +0800

    Update Map.vue

--- components/Map.vue
+++ components/Map.vue
@@ -17,7 +17,7 @@
       <iframe
         v-show="!loadFailed"
         class="absolute top-0 left-0 w-full h-full"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.1338458882262!2d121.01674635803198!3d14.55621982434462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c9a4ad5a62ef%3A0xc2765d4370d6f7d4!2sRufino%20Pacific%20Tower%2C%2037th%20Floor%2C%20678%20Ayala%20Ave%2C%20Legazpi%20Village%2C%20Makati%2C%201220%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1712800573432!5m2!1sen!2sph"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3861.717672763683!2d121.01642375924682!3d14.558130921838783!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c97daf02e46f%3A0xca28a24249260379!2sRufino%20Pacific%20Tower!5e0!3m2!1sen!2sph!4v1746777795510!5m2!1sen!2sph"
         style="border:0;"
         allowfullscreen="false"
         loading="lazy"
